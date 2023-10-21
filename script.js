2612343b218875678ae1ba2b45e8174f78981413
Sajjad Saberi <sajjad.saberi1996@gmail.com>
2023-10-21 22:35:57 +0330

    add another code for JS and CSS files

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const taskInput = document.getElementById("task-input") ;
 const dateInput = document.getElementById("date-input") ;
 const addButton = document.getElementById("add-button") ;
-
+const alertMessage = document.getElementById("alert-message") ;
 
 const todos = [] ;
+
+const showAlert = (message, type) => {
+    alertMessage.innerHTML = "";
+    const alert = document.createElement("p") ;
+    alert.innerText = message ;
+    alert.classList.add("alert") ;
+    alert.classList.add(`alert-${type}`); 
+    alertMessage.append(alert) ;
+
+    setTimeout(() => {
+        alert.style.display = "none" ;
+    }, 2000);
+};
 
 
 const addHandler = () => {
@@ -19,8 +32,9 @@ const addHandler = () => {
         taskInput.value = "" ;
         dateInput.value = "" ;
         console.log(todos) ;
+        showAlert("Todo Added Successfully", "success")
     } else {
-        alert("Warning")
+        showAlert("Please Enter a ToDo!", "error") ;
     }
 };
 
